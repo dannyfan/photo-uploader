5926db56f430ae859df716d1193632ed2675fb37
Danny Fan <dannyfan@live.com>
2020-09-16 16:59:31 -0400

    feat: add copy link to clipboard

--- src/components/ShareButton/ShareButton.js
+++ src/components/ShareButton/ShareButton.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './ShareButton.scss';
 
 const ShareButton = (props) => {
+    const inputRef = React.createRef();
     const visible = props.link ? '' : 'ShareButton-container__hidden';
+
+    const copyLink = () => {
+        inputRef.current.select();
+        document.execCommand('copy');
+    }
     return (
         <div className={`ShareButton-container ${visible}`}>
             <p>Share the link to your friends!</p>
-            <input type="text" value={props.link} readOnly/>
-            <button>Copy Link</button>
+            <input ref={inputRef} type="text" value={props.link} readOnly/>
+            <button onClick={copyLink}>Copy Link</button>
         </div>
     )
 };
